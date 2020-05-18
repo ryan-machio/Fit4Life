22bf8b68fcc862ddb3e3c393d0964b62fb458a8a
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-05-18 01:51:56 -0400

    Completed UserRegistration authentication and authorization functionality. Users are now required to create a profile to access the web app.

---
+++ Fit4Life/Areas/Identity/Data/Fit4LifeDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fit4Life.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fit4Life.Data
+{
+    public class Fit4LifeDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public Fit4LifeDbContext(DbContextOptions<Fit4LifeDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Fit4Life/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Fit4Life.Areas.Identity.Data;
+using Fit4Life.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Fit4Life.Areas.Identity.IdentityHostingStartup))]
+
+namespace Fit4Life.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<Fit4LifeDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Fit4LifeDbContextConnection")));
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                    {
+                        options.Password.RequireLowercase = false;
+                        options.Password.RequireUppercase = false;
+                        options.SignIn.RequireConfirmedAccount = false;
+                    })
+                    .AddEntityFrameworkStores<Fit4LifeDbContext>();
+            });
+        }
+    }
+}
