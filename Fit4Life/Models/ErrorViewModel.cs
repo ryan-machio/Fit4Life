3ad97018ab7847c2b776a867fd1a310966b74077
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-05-17 23:00:19 -0400

    First commit

---
+++ Fit4Life/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fit4Life.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
